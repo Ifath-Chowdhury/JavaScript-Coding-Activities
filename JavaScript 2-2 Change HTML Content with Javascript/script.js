4201c5000af601824924b4748016dacbad0c5fc5
ir2-chowdhury <Ifath2.Chowdhury@live.uwe.ac.uk>
2025-03-21 16:30:20 +0000

    Activity 2.2 for ITOnlineLearning JavaScript course completed

---
+++ JavaScript 2-2 Change HTML Content with Javascript/script.js
@@ -0,0 +1,7 @@
+function changeHeadingText() {
+    // Get the heading element with its id and assign it to a variable
+    var heading = document.getElementById("theHeading");
+
+    //Change the text of the heading element
+    heading.textContent = "The New Heading";
+}
